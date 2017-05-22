41894ffca9bf33793b5281d163f2d31a45a878f5
Mario Muniz <mario462@gmail.com>
2017-05-22 11:57:35 -0400

    First commit

---
+++ debmirror.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/bin/debmirror -p -v --nosource -h repos.uh.cu -e http -r ubuntu -d xenial,xenial-updates,xenial-security,xenial-backports -s main,restricted,multiverse,universe -a i386,amd64 --ignore-missing-release --ignore-release-gpg --diff=none --getcontents --i18n --rsync-extra=none $1
